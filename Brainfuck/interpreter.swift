21ff1752fda770d9b8c8aa0e3da19a7c45d89950
PointerFLY <linghao.michael.ma@gmail.com>
2019-08-04 20:54:37 +0800

    Make it reusable

--- Brainfuck/interpreter.swift
+++ Brainfuck/interpreter.swift
@@ -13,8 +13,8 @@ func runByText(_ text: String) {
     var pointer: Int = 0
     var loopStack = [Int]()
     var skipLoopCounter: Int = 0
-    
     var i = 0;
+    
     while i < text.count {
         let char = text[text.index(text.startIndex, offsetBy: i)]
         guard let token = Token(rawValue: char) else {
@@ -22,55 +22,12 @@ func runByText(_ text: String) {
             continue
         }
         
-        guard skipLoopCounter == 0 else {
-            if token == .loopStart {
-                skipLoopCounter += 1
-            } else if token == .loopEnd {
-                skipLoopCounter -= 1
-            }
-            i += 1
-            continue
-        }
-        
-        switch token {
-        case .left:
-            pointer -= 1
-            checkTapePointer(tape: &tape, pointer: pointer)
-        case .right:
-            pointer += 1
-            checkTapePointer(tape: &tape, pointer: pointer)
-        case .increase:
-            tape[pointer] &+= 1
-        case .decrease:
-            tape[pointer] &-= 1
-        case .loopStart:
-            if tape[pointer] == 0 {
-                skipLoopCounter = 1
-            } else {
-                loopStack.append(i)
-            }
-        case .loopEnd:
-            guard let startIndex = loopStack.popLast() else {
-                printError("illegal ] end without [")
-                exit(EXIT_FAILURE)
-            }
-            
-            if tape[pointer] != 0 {
-                i = startIndex - 1
-            }
-        case .input:
-            let data = FileHandle.standardInput.readData(ofLength: 1)
-            guard let value = String(data: data, encoding: .ascii)?.first?.asciiValue else {
-                printError("only ascii code is supported")
-                exit(EXIT_FAILURE)
-            }
-            tape[pointer] = value
-        case .output:
-            let char = Character(UnicodeScalar(tape[pointer]))
-            print(String(char), terminator: "")
-        }
-        
-        i += 1
+        operatePerStep(tape: &tape,
+                       pointer: &pointer,
+                       loopStack: &loopStack,
+                       skipLoopCounter: &skipLoopCounter,
+                       token: token,
+                       i: &i)
     }
 }
 
@@ -79,8 +36,8 @@ func runInteractively() {
     var pointer: Int = 0
     var loopStack = [Int]()
     var tokens = [Token]()
-    var i = 0
     var skipLoopCounter: Int = 0
+    var i = 0
     
     while (true) {
         var token: Token
@@ -89,66 +46,81 @@ func runInteractively() {
         } else {
             let data = FileHandle.standardInput.readData(ofLength: 1)
             guard let char = String(data: data, encoding: .ascii)?.first else { continue }
-            guard let t = Token(rawValue: char) else { continue }
-            token = t
+            guard let _token = Token(rawValue: char) else { continue }
+            token = _token
             tokens.append(token)
         }
         
-        guard skipLoopCounter == 0 else {
-            if token == .loopStart {
-                skipLoopCounter += 1
-            } else if token == .loopEnd {
-                skipLoopCounter -= 1
-            }
-            i += 1
-            continue
-        }
+        operatePerStep(tape: &tape,
+                       pointer: &pointer,
+                       loopStack: &loopStack,
+                       skipLoopCounter: &skipLoopCounter,
+                       token: token,
+                       i: &i)
+    }
+}
 
-        switch token {
-        case .left:
-            pointer -= 1
-            checkTapePointer(tape: &tape, pointer: pointer)
-        case .right:
-            pointer += 1
-            checkTapePointer(tape: &tape, pointer: pointer)
-        case .increase:
-            tape[pointer] &+= 1
-        case .decrease:
-            tape[pointer] &-= 1
-        case .loopStart:
-            if tape[pointer] == 0 {
-                skipLoopCounter = 1
-            } else {
-                loopStack.append(i)
-            }
-        case .loopEnd:
-            guard let startIndex = loopStack.popLast() else {
-                printError("illegal ] end without [")
-                exit(EXIT_FAILURE)
-            }
-            
-            if tape[pointer] != 0 {
-                i = startIndex - 1
-            }
-        case .input:
-            let data = FileHandle.standardInput.readData(ofLength: 1)
-            guard let value = String(data: data, encoding: .ascii)?.first?.asciiValue else {
-                printError("only ascii code is supported")
-                exit(EXIT_FAILURE)
-            }
-            tape[pointer] = value
-        case .output:
-            let char = Character(UnicodeScalar(tape[pointer]))
-            print(String(char), terminator: "")
+private func operatePerStep(tape: inout [UInt8],
+                            pointer: inout Int,
+                            loopStack: inout [Int],
+                            skipLoopCounter: inout Int,
+                            token: Token,
+                            i: inout Int) {
+    guard skipLoopCounter == 0 else {
+        if token == .loopStart {
+            skipLoopCounter += 1
+        } else if token == .loopEnd {
+            skipLoopCounter -= 1
+        }
+        i += 1
+        return
+    }
+    
+    switch token {
+    case .left:
+        pointer -= 1
+        checkTapePointer(tape: &tape, pointer: pointer)
+    case .right:
+        pointer += 1
+        checkTapePointer(tape: &tape, pointer: pointer)
+    case .increase:
+        tape[pointer] &+= 1
+    case .decrease:
+        tape[pointer] &-= 1
+    case .loopStart:
+        if tape[pointer] == 0 {
+            skipLoopCounter = 1
+        } else {
+            loopStack.append(i)
+        }
+    case .loopEnd:
+        guard let startIndex = loopStack.popLast() else {
+            printError("illegal ] end without [")
+            exit(EXIT_FAILURE)
         }
         
-        i += 1
+        if tape[pointer] != 0 {
+            i = startIndex - 1
+        }
+    case .input:
+        let data = FileHandle.standardInput.readData(ofLength: 1)
+        guard let value = String(data: data, encoding: .ascii)?.first?.asciiValue else {
+            printError("only ascii code is supported")
+            exit(EXIT_FAILURE)
+        }
+        tape[pointer] = value
+    case .output:
+        let char = Character(UnicodeScalar(tape[pointer]))
+        print(String(char), terminator: "")
     }
+    
+    i += 1
 }
 
 private func checkTapePointer(tape: inout [UInt8], pointer: Int) {
     if pointer < 0 {
         printError("tape pointer out of boundary")
+        exit(EXIT_FAILURE)
     } else if pointer >= tape.count {
         var newTape = [UInt8](repeating: 0, count: tape.count * 2)
         newTape.insert(contentsOf: tape, at: 0)
